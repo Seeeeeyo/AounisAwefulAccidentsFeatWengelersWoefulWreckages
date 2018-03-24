a94b3eb4875d7e1b75b7188530b9990928688ea0
swengeler <simon.wengeler@outlook.com>
2018-03-24 13:16:22 +0100

    Daily summary

--- subset_last_year.R
+++ subset_last_year.R
@@ -1,16 +1,12 @@
 # setting system locale for names in English
 Sys.setlocale("LC_TIME", "en_GB.utf8")
 
-# number of seconds in a year
-year_in_seconds <- 3.1536e+7
-
 # loading the original data set
 collisions <- as.data.frame(read_csv("NYPD_Motor_Vehicle_Collisions.csv"))
-collisions$NUMERIC_DATE <- lapply(collisions$DATE, function(x) as.numeric(as.POSIXct(x, format = "%m/%d/%Y")))
+collisions$POSIX_DATE <- lapply(collisions$DATE, function(x) as.POSIXct(x, format = "%m/%d/%Y"))
 
 # subsetting the data set
-last_year_time <- as.numeric(as.POSIXct("2018-03-23", format = "%Y-%m-%d")) - year_in_seconds
-last_year_collisions <- collisions[collisions$NUMERIC_DATE >= last_year_time, !(names(collisions) == "NUMERIC_DATE")]
+last_year_collisions <- collisions[collisions$POSIX_DATE >= as.POSIXct("2017-03-23", format = "%Y-%m-%d"), !(names(collisions) == "POSIX_DATE")]
 
 # doing some preprocessing for later use
 last_year_collisions$WEEKDAY <- unlist(lapply(last_year_collisions$DATE, function(x) weekdays(as.Date(x, format = "%m/%d/%Y"))))
